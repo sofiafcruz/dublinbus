50ced2647f7d8c7e53e1ceedb95e4dc60c1ce7ce
Conor Ginty <conorginty@gmail.com>
2020-06-18 17:59:08 +0100

    Created Bus App architechture and migrated DB

--- bustaTimes/bustaTimes/settings.py
+++ bustaTimes/bustaTimes/settings.py
@@ -31,8 +31,6 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    # Registering API
-    'dublinBusAPI.apps.DublinbusapiConfig', # Must follow this syntax
     # Boiler plate code
     'django.contrib.admin',
     'django.contrib.auth',
@@ -40,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # Our app
+    'bus_app'
 ]
 
 MIDDLEWARE = [
