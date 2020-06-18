9bef3719a922bd8611f8dfd92f1522dcd36d29fd
Conor Ginty <conorginty@gmail.com>
2020-06-18 14:46:10 +0100

    Setup serializers.py with template code

---
+++ bustaTimes/dublinBusAPI/serializers.py
@@ -0,0 +1,14 @@
+# This effectively acts as the form
+
+# from rest_framework import serializers
+# from .models import Routes, BusStops
+
+# class RoutesSerializer(serializers.ModelSerializer):
+#     class Meta:
+#         model = Routes
+#         fields = ['id', 'name', 'origin', 'destination', 'date_created']
+
+# class BusStopsSerializer(serializers.ModelSerializer):
+#     class Meta:
+#         model = Routes
+#         fields = ['id', 'pnum', 'stopID', 'stopname', 'lat', 'long']
