9a29854fa8c6ec1e74d08a75d42993d7475afe65
Conor Ginty <conorginty@gmail.com>
2020-06-18 18:22:27 +0100

    Created template files and images for testing

--- bustaTimes/bustaTimes/settings.py
+++ bustaTimes/bustaTimes/settings.py
@@ -14,7 +14,7 @@ import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
+TEMPLATE_DIR = os.path.join(BASE_DIR, 'blog_app/templates') # path created by to connect templates directory to the project
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
@@ -57,7 +57,7 @@ ROOT_URLCONF = 'bustaTimes.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [TEMPLATE_DIR, ], # We inserted TEMPLATE_DIR here
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -121,3 +121,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static') # To connect to the static folder
