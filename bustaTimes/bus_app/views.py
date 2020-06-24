075b16a225969983a6d490787a8836420b9652be
Conor Ginty <conorginty@gmail.com>
2020-06-24 21:44:29 +0100

    Added view to consider later for route access

--- bustaTimes/bus_app/views.py
+++ bustaTimes/bus_app/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
-
+from bus_app import models
 # Create your views here.
 
 def index(request):
     return render(request, 'index.html', {})
+
+# def get_route(request):
+#     value = request.["data"]
+#     models.Route.objects.filter("name"={})
