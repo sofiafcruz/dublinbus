c461dca885c03ec0a624fd34f8e1ef7393a590ab
Conor Ginty <conorginty@gmail.com>
2020-06-24 21:43:50 +0100

    Created Routes Model and registered it

--- bustaTimes/bus_app/admin.py
+++ bustaTimes/bus_app/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from bus_app.models import Route
+
+admin.site.register(Route)
--- bustaTimes/bus_app/models.py
+++ bustaTimes/bus_app/models.py
@@ -1,12 +1,12 @@
-# from django.db import models
+from django.db import models
 
 # # Create your models here.
 
-# class Route(models.Model):
-#     # Route ID defined automatically
-#     name = models.CharField(max_length=20) # Maybe this should be the ID/Primary Key instead?
-#     origin = models.CharField(max_length=100)
-#     destination = models.CharField(max_length=100)
+class Route(models.Model):
+    route_id = models.CharField(primary_key=True, max_length=20)
+    name = models.CharField(max_length=20) 
+    origin = models.CharField(max_length=100)
+    destination = models.CharField(max_length=100)
 
 #     def __str__(self):
 #         return self.name
@@ -16,7 +16,7 @@
 #     route = models.ForeignKey('bus_app.Route', on_delete=models.CASCADE)
 #     pnum = IntegerField()
 #     stopID = models.CharField(max_length=20) # Maybe this should be the ID/Primary Key instead?
-#     latitude = models.CharField(max_length=100)
+#     latitude = models.CharField(max_length=100)000
 #     longitude = models.CharField(max_length=100)
 
 #     def __str__(self):
