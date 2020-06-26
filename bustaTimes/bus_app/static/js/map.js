782cc07bc42394662ad403cb1a94dff368ee5313
Sofia Cruz <sofia.cruz@ucdconnect.ie>
2020-06-26 16:31:55 +0100

    Centered map on user's current location

--- bustaTimes/bus_app/static/js/map.js
+++ bustaTimes/bus_app/static/js/map.js
@@ -1,9 +1,20 @@
 function initMap() {
-    // Dublin Coordinates
-    var dublinCoords = {lat: 53.346, lng: -6.26};
-    // The map, centered at Dublin
-    var map = new google.maps.Map(
-        document.getElementById('map'), {zoom: 12, center: dublinCoords});
-    
-  }
 
+    var map = new google.maps.Map(document.getElementById('map'));
+  
+    navigator.geolocation.getCurrentPosition(function(position) {
+      // Center map on user's current location if geolocation prompt allowed
+      var initialLocation = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+      map.setCenter(initialLocation);
+      map.setZoom(15);
+      
+      var marker = new google.maps.Marker({
+        position: initialLocation,
+        map: map,
+      });
+    }, function(positionError) {
+      // Default to Dublin if user denied geolocation prompt
+      map.setCenter(new google.maps.LatLng(53.346, -6.26));
+      map.setZoom(12);
+    });
+  }
