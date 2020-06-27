2d1d5f03b22a485f83a3d65cf52d33cac64435a3
Conor Ginty <conorginty@gmail.com>
2020-06-27 20:24:37 +0100

    Included Route table in views.py and used logic to render all Route instances' attributes

--- bustaTimes/bus_app/views.py
+++ bustaTimes/bus_app/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-from bus_app import models
+from .models import Route, BusStop, RouteAndStop
+# From models.py file in the current folder, import the tables
 import os
 
 google_maps_key = os.environ.get("GOOGLEMAPS_KEY")
@@ -7,8 +8,14 @@ google_maps_key = os.environ.get("GOOGLEMAPS_KEY")
 # Create your views here.
 
 def index(request):
-    return render(request, 'index.html', {'google_maps_key':google_maps_key})
+    return render(request, 'index.html', {'google_maps_key':google_maps_key,
+                                          'routes': Route.objects.all(),
+                                           })
+
+def routes(request):
+    
+    return render(request, 'index.html', context)
 
 # def get_route(request):
 #     value = request.["data"]
-#     models.Route.objects.filter("name"={})
+#     Route.objects.filter("name"={})
