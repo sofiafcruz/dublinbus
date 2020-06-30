acef980ff9f825e28e9b10de585162498ff887ba
Sofia Cruz <sofia.cruz@ucdconnect.ie>
2020-06-30 21:04:21 +0100

    Added BusStop serializer

---
+++ bustaTimes/serializers.py
@@ -0,0 +1,7 @@
+from rest_framework import serializers
+from .models import BusStop
+
+class BusStopSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = BusStop
+        fields = '__all__'
