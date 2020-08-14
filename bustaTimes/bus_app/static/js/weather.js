c4260e910f60fffdb87d2ed97ab5ea7b7043839a
Sofia Cruz <sofia.cruz@ucdconnect.ie>
2020-08-14 11:43:03 +0100

    Added some more comments

--- bustaTimes/bus_app/static/js/weather.js
+++ bustaTimes/bus_app/static/js/weather.js
@@ -20,18 +20,9 @@ if (typeof temperature === 'number') {
             document.querySelector('.alert-rain').style.display = 'none';
         }, 10000);
     }
-    // 
-    // console.log(iconName);
-    // // Check if there's icon for the current weather
-    // var url = './static/images/weather/' + iconName + '.png';
-    // console.log(url);
-    // $.get(url)
-    //     .done(function() { 
-    //         document.getElementById('temperature').innerHTML += '<img src="' + url + '" alt="' + iconName + '" style="width:40px">'; 
-    //     }).fail(function() { 
-    //         document.getElementById('temperature').innerHTML += '<img src="./static/images/unknown.png" alt="' + iconName + '" style="width:40px">'; 
-    //     })
+
 } else {
+    // If cannot get temperature leave container empty
     document.getElementById('weather-container').innerHTML = '';
 }
 
